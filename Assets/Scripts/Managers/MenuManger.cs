2f27d105b43017fcac1c1a2c8870256f60aed1cd
4419-3-7\user <iyguigil>
2017-10-28 14:49:51 +0800

    增加UI介面

---
+++ Assets/Scripts/Managers/MenuManger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManger : MonoBehaviour {
+    private Canvas canvas;
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Canvas.enbled = !Canvas.enbled;
+            Pause();
+        }
+		
+	}
+    public void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+       
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
